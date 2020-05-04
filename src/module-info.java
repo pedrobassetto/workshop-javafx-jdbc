31332c9099602000ff3b12b92f9d5aa9653a297c
Pedro Bassetto <contato@pedrobassetto.com>
2020-05-04 19:31:51 -0300

    Inclusão de Banco de Dados

--- src/module-info.java
+++ src/module-info.java
@@ -2,10 +2,14 @@ module workshop.javafx.jdbc {
 
     requires javafx.fxml;
     requires javafx.controls;
+    requires java.sql;
 
     opens application;
+    opens db;
     opens gui;
     opens gui.util;
+    opens model.dao;
     opens model.entities;
+    opens model.services;
 
 }
